ce13eda6a66f2945e65af330d82075e56c7ca8c7
YosephKurubinganBekayo <bekayokurnia@gmail.com>
2025-01-08 15:59:14 +0700

    bug tombol

--- index.php
+++ index.php
@@ -127,8 +127,6 @@ require 'call_fungtion.php';
             ?>
 
 
-          </div>
-          <div>
             <a href="about.php?id=<?php echo urlencode($data_gambar['id_aboutus']); ?>" class="btn btn-secondary rounded-end rounded-top py-2 px-5 flex-shrink-0">Selengkapnya</a>
           </div>
         </div>
